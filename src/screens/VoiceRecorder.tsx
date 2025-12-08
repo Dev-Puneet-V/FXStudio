1dcfea6b281be7ebb39f215a230b83adc45be577
puneet <imhtcy312@gmail.com>
2025-12-09 05:26:26 +0530

    [DEV-EXP] Setup RN Repo

---
+++ src/screens/VoiceRecorder.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const VoiceRecorder = () => {
+    return (
+        <View>
+            <Text>VoiceRecorder</Text>
+        </View>
+    )
+}
+
+export default VoiceRecorder;
